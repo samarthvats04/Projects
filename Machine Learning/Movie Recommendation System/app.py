a1439cfcbf3ef0ba113ae3b4d1c81e65b77a1274
Samarth Vats <143289583+samarthvats04@users.noreply.github.com>
2024-11-23 21:50:46 +0530

    Update app.py

--- Machine Learning/Movie Recommendation System/app.py
+++ Machine Learning/Movie Recommendation System/app.py
@@ -1,17 +1,26 @@
+#import required libraries
 import streamlit as st
 import pickle
 import pandas as pd
 import requests
 
+#function to display movie posters
 def fetch_poster(movie_id):
+	#fetch data of movie poster using the id tag through TMDB API
 	response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
 	data = response.json()
 	return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+	
+#function to fetch the recommended movies	
 def recommend(movie):
 	movie_index = movies[movies['title'] == movie].index[0]
+	#distances to show the degree of similarity between the input movie and the output movies
 	distances = similarity[movie_index]
+	'''return the movies in a sorted manner, 
+	where the most similar one is displayed first down to the least similar out of the top 5'''
 	movies_list = sorted(list(enumerate(distances)),reverse=True, key=lambda x:x[1])[1:6]
 
+	#display the movies along with the posters
 	recommended_movies = []
 	recommended_posters = []
 	for i in movies_list:
@@ -20,22 +29,25 @@ def recommend(movie):
 		recommended_posters.append(fetch_poster(movie_id))
 	return recommended_movies, recommended_posters
 
+#pipelining the dataset and the similarity matrix using a pkl file into the streamlit app
 movies_dict = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
 similarity = pickle.load(open('similarity.pkl','rb'))
 
 
-
-
 st.title('Movie Recommendation System')
+
+#adding a selection box for title selection. Title can be typed in the placeholder as well.
 selected_movie = st.selectbox(
 	'Recommended movies:',
 	movies['title'].values
 )
 
+
 if st.button('Find Similar Movies'):
 	names, posters = recommend(selected_movie)
 
+	#displaying the movies and the posters in a columnar layout
 	col1, col2, col3, col4, col5 = st.columns(5)
 	with col1:
 		st.text(names[0])
@@ -52,3 +64,5 @@ if st.button('Find Similar Movies'):
 	with col5:
 		st.text(names[4])
 		st.image(posters[4])
+
+#run the app locally on your web browser by passing the commands: 'streamlit run app.py' in the terminal
